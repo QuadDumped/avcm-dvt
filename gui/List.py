33c2ededafb29a51c365e9d06ec3507edcbcc664
AlvinLarsson <42584015+QuadDumped@users.noreply.github.com>
2021-03-18 20:49:21 +0100

    Integrerade graf i GUI

--- gui/List.py
+++ gui/List.py
@@ -1,20 +1,37 @@
 import tkinter
 from tkinter import *
+from matplotlib.figure import Figure 
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
-top = Tk()
+def embeddedPlot(X, Y):
+    fig = Figure(figsize = (5, 5), dpi = 100) 
+    plot1 = fig.add_subplot(111) 
+    plot1.plot(X, Y)
 
-mb=  Menubutton (top, text="ABOUT US", relief=RAISED)
-mb.grid()
+    canvas = FigureCanvasTkAgg(fig, master = window)   
+    canvas.draw() 
+    canvas.get_tk_widget().pack()
+
+x = [1, 2, 3, 4]
+y = [1, 2, 3, 6]
+
+window = Tk()
+window.geometry("600x300")
+window.title("A graph") 
+
+mb = Menubutton (window, text="Signals", relief=RAISED)
 mb.menu =  Menu ( mb, tearoff = 0 )
 mb["menu"] =  mb.menu
 
 mayoVar = IntVar()
 ketchVar = IntVar()
+mb.menu.add_checkbutton ( label="Signal 1", variable=mayoVar)
+mb.menu.add_checkbutton ( label="Signal 2", variable=ketchVar)
+mb.pack(side = TOP, anchor=NW)
 
-mb.menu.add_checkbutton ( label="Mikke",
-                       variable=mayoVar )
-mb.menu.add_checkbutton ( label="Pikke",
-                          variable=ketchVar )
+embeddedPlot(x, y)
 
-mb.pack()
-top.mainloop()
+window.mainloop()
+
+
