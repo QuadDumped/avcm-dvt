79f982378bab17a3d6a43e9d7250b5d51256eff7
AlvinLarsson <42584015+QuadDumped@users.noreply.github.com>
2021-03-18 12:51:55 +0100

    ny plotfunktion

--- plot.py
+++ plot.py
@@ -37,6 +37,34 @@ def plotFunction(dataList, maxValues):
   plt.plot(x, y)
   plt.show()
 
+#Xincrement är om x ska ökas med 1 för varje värde i dataList, eller som det ska hämtas direkt som ett x värde 
+#X och Y-index väljer vilka indexes i värdet som anger x och y
+def plotFunctionPlus(dataList, maxValues = math.inf, Xincrement = True, Yindex = 0, Xindex = None):
+  x = []
+  y = []
+
+  i=0
+
+  for row in dataList:
+    i+=1
+    if len(x) < maxValues:
+      
+      if Xincrement == True:
+        x.append(i)
+      else:
+        x.append(row[Xindex])
+
+      y.append(row[Yindex])
+
+  plt.plot(x, y)
+  plt.show()
+
+
+
+
+
+
 #exempel
 #plotFunction(groupItem("ttTask"))
 #plotFunction(groupItem("ttTask"), 1000)
+#plotFunctionPlus(groupItem("ttTask"), 1000, True, 1)
