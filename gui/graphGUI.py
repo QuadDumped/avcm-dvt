d014be1f3f2e66901e0c61458c7c54d019678629
AlvinLarsson <42584015+QuadDumped@users.noreply.github.com>
2021-03-22 14:11:33 +0100

    Embedded graph in GUI

--- gui/graphGUI.py
+++ gui/graphGUI.py
@@ -1,13 +1,33 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QComboBox
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
+from matplotlib.figure import Figure
+
+
+def selectedItem(item):
+    print(item)
 
 app = QApplication([])
 app.setStyle("Fusion")
 
 window = QWidget()
 layout = QVBoxLayout()
-layout.addWidget(QPushButton("Button"))
-window.setLayout(layout)
 
+combo = QComboBox()
+combo.addItem("Signal 1")
+combo.addItem("Signal 2")
+combo.addItem("Signal 3")
+combo.activated[str].connect(selectedItem) 
+layout.addWidget(combo)
+
+fig = Figure(figsize=(4, 5), dpi=100)
+axes = fig.add_subplot(111)
+axes.plot([0,1,2,3,4], [10,1,20,3,40])
+
+canvas = FigureCanvasQTAgg(fig) 
+layout.addWidget(canvas)
+
+
+window.setLayout(layout)
 window.show()
 app.exec()
 
