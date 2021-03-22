1bb2686d073fc64ebbe4ec29c2e54f0531cf694f
AlvinLarsson <42584015+QuadDumped@users.noreply.github.com>
2021-03-22 15:37:33 +0100

    uppdaterade graphGUI

--- gui/graphGUI.py
+++ gui/graphGUI.py
@@ -1,10 +1,21 @@
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QComboBox
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
 from matplotlib.figure import Figure
+#from h5reader import groupItem, groupStructure
 
+path = "log_210129_124838_1611920928.h5"
 
 def selectedItem(item):
     print(item)
+
+def plot(itemdata):
+
+    fig = Figure(figsize=(4, 5), dpi=100)
+    axes = fig.add_subplot(111)
+    axes.plot([0,1,2,3,4], [10,1,20,3,40])
+    canvas = FigureCanvasQTAgg(fig) 
+    return canvas
+
 
 app = QApplication([])
 app.setStyle("Fusion")
@@ -13,20 +24,20 @@ window = QWidget()
 layout = QVBoxLayout()
 
 combo = QComboBox()
-combo.addItem("Signal 1")
-combo.addItem("Signal 2")
-combo.addItem("Signal 3")
+groupItems = groupStructure('0 VCM1')
+for item in groupItems:
+    combo.addItem(item)
+
+#hämta data från valt item och skicka in det i plot
+currentItem = combo.currentText
+itemData = "null" #groupItem(currentItem)
+canvas = plot(itemData)
+
+#kopplar selectedItem till knapparna och skriver ut item i terminalen
 combo.activated[str].connect(selectedItem) 
+
 layout.addWidget(combo)
-
-fig = Figure(figsize=(4, 5), dpi=100)
-axes = fig.add_subplot(111)
-axes.plot([0,1,2,3,4], [10,1,20,3,40])
-
-canvas = FigureCanvasQTAgg(fig) 
 layout.addWidget(canvas)
-
-
 window.setLayout(layout)
 window.show()
 app.exec()
